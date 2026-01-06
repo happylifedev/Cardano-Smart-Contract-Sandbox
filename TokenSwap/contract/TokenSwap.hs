abd7376f71d1fb35502d65e2c857fd36b8de8e51
Happy <happyshr10.dekiru@gmail.com>
2026-01-06 11:25:05 +0900

    Update TokenSwap.hs

--- TokenSwap/contract/TokenSwap.hs
+++ TokenSwap/contract/TokenSwap.hs
@@ -1,56 +1,100 @@
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module TokenSwap where
 
-import           Plutus.V2.Ledger.Api      (
-                                           ScriptContext (scriptContextTxInfo), PubKeyHash, Validator,
-                                           mkValidatorScript, adaToken, adaSymbol, singleton,
-                                           TxInInfo (txInInfoResolved),
-                                           TxInfo (txInfoInputs, txInfoOutputs),
-                                           TxOut (txOutAddress), Address (addressCredential), Credential (PubKeyCredential)
-                                           )
-import           Plutus.V2.Ledger.Contexts (valuePaidTo, ownHash)
-import           PlutusTx                  (compile, unstableMakeIsData)
-import           PlutusTx.Builtins         (BuiltinData, Integer)
-import           PlutusTx.Prelude          (Bool (..), (==), traceIfFalse, (&&), length, filter, map)
-import           Plutus.V1.Ledger.Address  (scriptHashAddress)
-import           Utilities                 (wrapValidator, writeValidatorToFile)
-import           Prelude                   (IO, (||), all)
+import Plutus.V2.Ledger.Api
+    ( ScriptContext (scriptContextTxInfo)
+    , Validator
+    , PubKeyHash
+    , mkValidatorScript
+    , adaSymbol
+    , adaToken
+    , singleton
+    , TxInfo (txInfoInputs)
+    , TxInInfo (txInInfoResolved)
+    , TxOut (txOutAddress)
+    )
 
+import Plutus.V2.Ledger.Contexts
+    ( valuePaidTo
+    , ownHash
+    )
 
-data DatumSwap = DatumSwap { seller :: PubKeyHash
-                           , price  :: Integer
-                           }
+import PlutusTx
+    ( compile
+    , unstableMakeIsData
+    )
+
+import PlutusTx.Prelude
+    ( Bool (..)
+    , Integer
+    , (>=)
+    , (==)
+    , traceIfFalse
+    , length
+    , filter
+    )
+
+import Plutus.V1.Ledger.Address (scriptHashAddress)
+import Utilities (wrapValidator, writeValidatorToFile)
+
+-------------------------------------------------
+-- Datum
+-------------------------------------------------
+
+data DatumSwap = DatumSwap
+    { seller :: PubKeyHash
+    , price  :: Integer
+    }
+
 PlutusTx.unstableMakeIsData ''DatumSwap
+
+-------------------------------------------------
+-- Validator
+-------------------------------------------------
 
 {-# INLINABLE mkValidator #-}
 mkValidator :: DatumSwap -> () -> ScriptContext -> Bool
-mkValidator ds _ ctx = (traceIfFalse "You have to pay the seller!" outputToSeller &&
-                        traceIfFalse "Can only consume one utxo at a time" singleOutputConsumed)
-                       || consumerIsSeller -- future: make sure that seller can't retrieve the nft once a winner has won
-    where
-        txInfo :: TxInfo
-        txInfo = scriptContextTxInfo ctx
+mkValidator ds _ ctx =
+       traceIfFalse "Seller not paid enough ADA" sellerPaid
+    && traceIfFalse "Only one script input allowed" singleScriptInput
+  where
+    info = scriptContextTxInfo ctx
 
-        outputToSeller :: Bool
-        outputToSeller =
-          valuePaidTo txInfo (seller ds) == singleton adaSymbol adaToken (price ds)
-        
-        singleOutputConsumed :: Bool
-        singleOutputConsumed = length (filter (\x -> txOutAddress x == scriptHashAddress (ownHash ctx)) (map txInInfoResolved (txInfoInputs txInfo))) == 1
+    -- ✅ Accepts payment >= price
+    sellerPaid :: Bool
+    sellerPaid =
+        valuePaidTo info (seller ds)
+            `geq` singleton adaSymbol adaToken (price ds)
 
-        consumerIsSeller :: Bool
-        consumerIsSeller = all (\o -> addressCredential (txOutAddress o) == PubKeyCredential (seller ds)) (txInfoOutputs txInfo)
+    -- ✅ Ensure exactly one script UTXO is consumed
+    singleScriptInput :: Bool
+    singleScriptInput =
+        length (filter isOwnInput (txInfoInputs info)) == 1
 
-{-# INLINABLE  mkWrappedValidator #-}
+    isOwnInput :: TxInInfo -> Bool
+    isOwnInput i =
+        txOutAddress (txInInfoResolved i)
+            == scriptHashAddress (ownHash ctx)
+
+-------------------------------------------------
+-- Boilerplate
+-------------------------------------------------
+
+{-# INLINABLE mkWrappedValidator #-}
 mkWrappedValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkWrappedValidator = wrapValidator mkValidator
 
 validator :: Validator
-validator = mkValidatorScript $$(compile [|| mkWrappedValidator ||])
+validator =
+    mkValidatorScript $$(compile [|| mkWrappedValidator ||])
 
------ helpers
+-------------------------------------------------
+-- Helper
+-------------------------------------------------
+
 writeValidator :: IO ()
-writeValidator = writeValidatorToFile "./assets/tokenswap.plutus" validator
+writeValidator =
+    writeValidatorToFile "./assets/tokenswap.plutus" validator
